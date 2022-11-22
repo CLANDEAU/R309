49c72f69c5fe8adeeaac41c99f68c5a883a80336
CLANDEAU <92865093+CLANDEAU@users.noreply.github.com>
2022-11-22 23:53:24 +0100

    setup

--- SAE302/Client.py
+++ SAE302/Client.py
@@ -1,29 +1,35 @@
 import socket
 import threading
-host="127.0.0.1"
-port= 8000
-client_socket = socket.socket()
-client_socket.connect((host, port))
-def echange():
-    com = True
-    while com:
-        message = input("Message envoyé= ")
-        if message == "disconnect":
-            com = False
-            client_socket.send(message.encode())
-        elif message == "reset":
-            com = False
-            client_socket.send(message.encode())
-        elif message == "kill":
-            com = False
-            client_socket.send(message.encode())
-        else:
-            client_socket.send(message.encode())
-            data = client_socket.recv(1024).decode()
-            if data == "bye":
-                com = False
-            else:
-                print(f"Message reçu= {data}")
-    client_socket.close()
 
-echange = threading.Thread(target=echange)
+class Socket:
+    def __init__(self,host="127.0.0.1",port=8000):
+        self.__host=host
+        self.__port = port
+        self.__client_socket = socket.socket()
+        self.__client_socket.connect((host, port))
+
+    def echange(self):
+        com = True
+        while com:
+            message = input("Message envoyé=")
+            if message == "disconnect":
+                com = False
+                self.__client_socket.send(message.encode())
+            elif message == "kill":
+                com = False
+                self.__client_socket.send(message.encode())
+            elif message == "reset":
+                print("pas encore en action")
+            else:
+                self.__client_socket.send(message.encode())
+                data = self.__client_socket.recv(1024).decode()
+                if data == "disconnect":
+                    com = False
+                else:
+                    print(data)
+
+
+socket1=Socket()
+echange = threading.Thread(target=socket1.echange)
+echange.start()
+echange.join()
--- SAE302/Server.py
+++ SAE302/Server.py
@@ -1,4 +1,7 @@
 import socket
+import psutil
+import platform
+import subprocess
 
 host="127.0.0.1"
 port= 8000
@@ -10,10 +13,53 @@ conn, address = server_socket.accept()
 
 while com:
     data = conn.recv(1024).decode()
+    OS = platform.uname()
     if data == "disconnect":
         print(f"Message reçu= {data}")
         conn, address = server_socket.accept()
-    elif data == "kill":
-        conn.close()
+    elif data == "OS":
+        conn.send(f"La machine utilise le système d'exploitation: {OS[0]}".encode())
+    elif data == "RAM":
+        RAM = psutil.virtual_memory()
+        conn.send(f"La RAM totale est d'environ: {round(RAM[0]/10**9,1)}, la RAM utilisé est d'environ: {round(RAM[3]/10**9,1)}, la RAM restante est d'environ: {round(RAM[4]/10**9,1)}".encode())
+    elif data == "CPU":
+        conn.send(f"Actuellement, le CPU utilise: {psutil.cpu_percent(1)}% de ses capacités.".encode())
+    elif data == "IP":
+        conn.send(f"L'adresse IP de la machine est: {socket.gethostbyname(socket.gethostname())}".encode())
+    elif data == "Name":
+        conn.send(f"Le nom de la machine est: {OS[1]}".encode())
+    elif data == "Connexion information":
+        conn.send(f"L'adresse IP de la machine est: {socket.gethostbyname(socket.gethostname())} et le nom de la machine est {OS[1]}".encode())
+
+    elif data == "Powershell:get-process":
+        if OS[0] == "Windows":
+            command = subprocess.run(["powershell", "-Command", "Get-Process"], capture_output=True)
+            result=command.stdout.decode('windows-1252')
+            print(result)
+            conn.send(f"Voici le résultat de la commmande get-process: {result}".encode())
+        else:
+            conn.send("Le système d'exploitation est incorrect.".encode())
+    elif data == "ls -la":
+        if OS[0] == "Linux":
+            print("pas encore fait")
+        else:
+            conn.send("Le système d'exploitation est incorrect.".encode())
+    elif data == "DOS:dir":
+        if OS[0] == "Mac-OS":
+            print("pas encore fait")
+        else:
+            conn.send("Le système d'exploitation est incorrect.".encode())
+    elif data == "DOS:mkdir toto":
+        if OS[0] == "Mac-OS":
+            print("pas encore fait")
+        else:
+            conn.send("Le système d'exploitation est incorrect.".encode())
+    elif data == "Powershell:get-process":
+        if OS[0] == "Mac-OS":
+            print("pas encore fait")
+        else:
+            conn.send("Le système d'exploitation est incorrect.".encode())
+
     else:
         print(f"Message reçu= {data}")
+
