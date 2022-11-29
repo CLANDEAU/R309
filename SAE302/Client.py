3a129d1bf128cd01be2b00ea8e73211c49d27bd1
CLANDEAU <92865093+CLANDEAU@users.noreply.github.com>
2022-11-29 17:26:03 +0100

    big update

--- SAE302/Client.py
+++ SAE302/Client.py
@@ -7,111 +7,84 @@ import sys
 from PyQt5.QtWidgets import QApplication
 
 class Socket:
-    def __init__(self,host="127.0.0.1",port=8004):
+    def __init__(self,host="127.0.0.1",port=8005):
         self.__host=host
         self.__port = port
         self.__client_socket = socket.socket()
-        self.__client_socket.connect((host, port))
-
-    def echange(self):
+        message = "Connecté"
+        self.__message = message
+    def get_msg(self):
+        return self.__message
+    def set_msg(self,msg):
+        self.__message = msg
+    def connect(self):
+        self.__client_socket.connect((self.__host, self.__port))
+    def echange(self,message):
         com = True
-        while com:
-            message = input("Message envoyé=")
-            if message == "disconnect":
+        #message = input("Message envoyé=")
+        if self.__message == "disconnect":
+            com = False
+            self.__client_socket.send(self.__message.encode())
+        elif self.__message == "kill":
+            com = False
+            self.__client_socket.send(self.__message.encode())
+        elif self.__message == "reset":
+            self.__client_socket.send(self.__message.encode())
+        else:
+            self.__client_socket.send(self.__message.encode())
+            data = self.__client_socket.recv(100000).decode()
+            if data == "disconnect":
                 com = False
-                self.__client_socket.send(message.encode())
-            elif message == "kill":
-                com = False
-                self.__client_socket.send(message.encode())
-            elif message == "reset":
-                self.__client_socket.send(message.encode())
+            elif data == "ping 192.157.65.78":
+                print("Voici le résultat de la commmande ping 192.157.65.78:")
+                print(subprocess.Popen('ping 192.157.65.78'))
             else:
-                self.__client_socket.send(message.encode())
-                data = self.__client_socket.recv(100000).decode()
-                if data == "disconnect":
-                    com = False
-                elif data == "ping 192.157.65.78":
-                    print("Voici le résultat de la commmande ping 192.157.65.78:")
-                    subprocess.Popen('ping 192.157.65.78')
-                else:
-                    print(data)
+                return data
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
         widget = QWidget()
         self.setCentralWidget(widget)
 
         grid = QGridLayout()
         widget.setLayout(grid)
 
-        self.__temp = QLabel("Temperature")
-        self.__text = QLineEdit("")
-        self.__temp2 = QLabel("°C")
-        self.__temp3 = QLabel("Conversion")
-        self.__temp4 = QLabel("K")
-        self.__conv = QPushButton("Convertir")
-        self.__interrogation = QPushButton("?")
-        self.__answer = QLineEdit("")
-        self.__answer.setEnabled(False)
-        self.__cb = QComboBox()
-        self.__cb.addItem("°C -> K")
-        self.__cb.addItem("K -> °C")
-        self.__msgBox=QMessageBox()
-        self.__msgBox.setText("Permet de convertir un nombre soit de Kelvin vers Celcius, soit de Celcius vers Kelvin")
-        self.__msgBox.setWindowTitle("Aide")
-        self.__msgBox2 = QMessageBox()
-        self.__msgBox2.setText("Erreur, il faut entrer un float.")
-        self.__msgBox2.setWindowTitle("Erreur")
-        grid.addWidget(self.__temp, 0, 0)
-        grid.addWidget(self.__temp2, 0, 2)
-        grid.addWidget(self.__text, 0,1)
-        grid.addWidget(self.__conv, 1,1)
-        grid.addWidget(self.__cb, 1, 2)
-        grid.addWidget(self.__answer, 2, 1)
-        grid.addWidget(self.__temp3, 2, 0)
-        grid.addWidget(self.__temp4, 2, 2)
-        grid.addWidget(self.__interrogation, 3, 3)
-
-
-        self.__conv.clicked.connect(self._actionConv)
-        self.__interrogation.clicked.connect(self._actionInterrogation)
-        self.__cb.currentTextChanged.connect(self._Combobox_change)
-
+        self.setGeometry(0, 0, 1920, 1080)
+        self.__print = QLineEdit("print")
         self.setWindowTitle("Charles")
-    def _actionConv(self):
-        try:
-            temp_recup=float(self.__text.text())
-        except:
-            self.__msgBox2.exec()
-            self.__text.setText("")
-        else:
-            if self.__cb.currentText() == "°C -> K":
-                res=temp_recup+273.15
-                self.__answer.setText(f"{res}")
-            else:
-                res=temp_recup-273.15
-                self.__answer.setText(f"{res}")
-    def _actionInterrogation(self):
-        self.__msgBox.exec()
-    def _Combobox_change(self):
-        if self.__cb.currentText()=="°C -> K":
-            self.__temp2.setText("°C")
-            self.__temp4.setText("K")
-        else:
-            self.__temp4.setText("°C")
-            self.__temp2.setText("K")
+        self.__ter=QLabel("")
 
+        grid.addWidget(self.__print, 0, 0)
+        grid.addWidget(self.__ter, 0, 1)
+        self.__conv = QPushButton("Se connecter")
+        self.__conv.clicked.connect(self.connection)
+        self.__send = QPushButton("Envoyer")
+        grid.addWidget(self.__conv, 1, 0)
+        self.__send.clicked.connect(self.send)
+        grid.addWidget(self.__send, 1, 1)
+    def connection(self):
+        self.__soc=Socket()
+        self.__soc.connect()
+    def send(self):
+        msg=Socket.set_msg(self.__soc,self.__print.text())
+        self.__ter.setText(Socket.echange(self.__soc, msg))
 app = QApplication(sys.argv)
 window = MainWindow()
 window.show()
 app.exec()
+
+
+
+
+
+"""
 
 socket1=Socket()
 echange = threading.Thread(target=socket1.echange)
 echange.start()
 echange.join()
 
-
+args=[Socket.get_msg]
+"""
--- SAE302/Server.py
+++ SAE302/Server.py
@@ -9,79 +9,78 @@ def restart():
     print("sys.executable was", sys.executable)
     print("restart now")
     os.execv(sys.executable, ['python'] + sys.argv)
-try:
-    host="127.0.0.1"
-    port= 8004
-    com=True
-    server_socket = socket.socket()
-    server_socket.bind((host, port))
-    server_socket.listen(1)
-    conn, address = server_socket.accept()
 
-    while com:
-        data = conn.recv(1024).decode()
-        OS = platform.uname()
-        if data == "disconnect":
-            print(f"Message reçu= {data}")
-            conn, address = server_socket.accept()
-        if data == "reset":
-            restart()
-        elif data == "OS":
-            conn.send(f"La machine utilise le système d'exploitation: {OS[0]}".encode())
-        elif data == "RAM":
-            RAM = psutil.virtual_memory()
-            conn.send(f"La RAM totale est d'environ: {round(RAM[0]/10**9,1)}Gb, la RAM utilisé est d'environ: {round(RAM[3]/10**9,1)}Gb, la RAM restante est d'environ: {round(RAM[4]/10**9,1)}Gb".encode())
-        elif data == "CPU":
-            conn.send(f"Actuellement, le CPU utilise: {psutil.cpu_percent(1)}% de ses capacités.".encode())
-        elif data == "IP":
-            conn.send(f"L'adresse IP de la machine est: {socket.gethostbyname(socket.gethostname())}".encode())
-        elif data == "Name":
-            conn.send(f"Le nom de la machine est: {OS[1]}".encode())
-        elif data == "Connexion information":
-            conn.send(f"L'adresse IP de la machine est: {socket.gethostbyname(socket.gethostname())} et le nom de la machine est {OS[1]}".encode())
+host="127.0.0.1"
+port= 8005
+com=True
+server_socket = socket.socket()
+server_socket.bind((host, port))
+server_socket.listen(1)
+conn, address = server_socket.accept()
 
-        elif data == "Powershell:get-process":
-            if OS[0] == "Windows":
-                command = subprocess.run(["powershell", "-Command", "Get-Process"], capture_output=True)
-                result=command.stdout.decode('windows-1252')
-                print(result)
-                conn.send(f"Voici le résultat de la commmande get-process: {result}".encode())
-            else:
-                conn.send("Le système d'exploitation est incorrect.".encode())
-        elif data == "Linux:ls -la":
-            if OS[0] == "Linux":
-                command = subprocess.run(["ls","-la"], capture_output=True)
-                result = command.stdout.decode('windows-1252')
-                conn.send(f"Voici le résultat de la commmande ls -la: {result}".encode())
-            else:
-                conn.send("Le système d'exploitation est incorrect.".encode())
-        elif data == "DOS:dir":
-            if OS[0] == "Windows":
-                command = subprocess.run(["-Command", "dir"], capture_output=True)
-                result = command.stdout.decode('windows-1252')
-                conn.send(f"Voici le résultat de la commmande dir: {result}".encode())
-            else:
-                conn.send("Le système d'exploitation est incorrect.".encode())
-        elif data == "DOS:mdir toto":
-            if OS[0] == "Windows":
-                command = subprocess.run(["mdir", "toto"], capture_output=True)
-                result = command.stdout.decode('windows-1252')
-                conn.send(f"Voici le résultat de la commmande mdir toto: {result}".encode())
-            else:
-                conn.send("Le système d'exploitation est incorrect.".encode())
-        elif data == "python --version":
-            command = subprocess.run(["python", "--version"], capture_output=True)
-            result=command.stderr.decode('windows-1252')
-            conn.send(f"Voici le résultat de la commmande python --version: {result}".encode())
-        elif data == "ping 192.157.65.78":
-            if OS[0] == "Windows":
-                conn.send("ping 192.157.65.78".encode())
-            else:
-                command = subprocess.run(["ping","192.157.65.78","-c","1","-W","2"],capture_output=True)
-                result=command.stdout.decode()
-                conn.send(f"Voici le résultat de la commmande ping 192.157.65.78: {result}".encode())
+while com:
+    data = conn.recv(1024).decode()
+    OS = platform.uname()
+    if data == "disconnect":
+        print(f"Message reçu= {data}")
+        conn, address = server_socket.accept()
+    if data == "reset":
+        restart()
+    elif data == "OS":
+        conn.send(f"La machine utilise le système d'exploitation: {OS[0]}".encode())
+    elif data == "RAM":
+        RAM = psutil.virtual_memory()
+        conn.send(f"La RAM totale est d'environ: {round(RAM[0]/10**9,1)}Gb, la RAM utilisé est d'environ: {round(RAM[3]/10**9,1)}Gb, la RAM restante est d'environ: {round(RAM[4]/10**9,1)}Gb".encode())
+    elif data == "CPU":
+        conn.send(f"Actuellement, le CPU utilise: {psutil.cpu_percent(1)}% de ses capacités.".encode())
+    elif data == "IP":
+        conn.send(f"L'adresse IP de la machine est: {socket.gethostbyname(socket.gethostname())}".encode())
+    elif data == "Name":
+        conn.send(f"Le nom de la machine est: {OS[1]}".encode())
+    elif data == "Connexion information":
+        conn.send(f"L'adresse IP de la machine est: {socket.gethostbyname(socket.gethostname())} et le nom de la machine est {OS[1]}".encode())
 
+    elif data == "Powershell:get-process":
+        if OS[0] == "Windows":
+            command = subprocess.run(["powershell", "-Command", "Get-Process"], capture_output=True)
+            result=command.stdout.decode('windows-1252')
+            print(result)
+            conn.send(f"Voici le résultat de la commmande get-process: {result}".encode())
         else:
-            print(f"Message reçu= {data}")
-except:
-    restart()
+            conn.send("Le système d'exploitation est incorrect.".encode())
+    elif data == "Linux:ls -la":
+        if OS[0] == "Linux":
+            command = subprocess.run(["ls","-la"], capture_output=True)
+            result = command.stdout.decode('windows-1252')
+            conn.send(f"Voici le résultat de la commmande ls -la: {result}".encode())
+        else:
+            conn.send("Le système d'exploitation est incorrect.".encode())
+    elif data == "DOS:dir":
+        if OS[0] == "Windows":
+            command = subprocess.run(["-Command", "dir"], capture_output=True)
+            result = command.stdout.decode('windows-1252')
+            conn.send(f"Voici le résultat de la commmande dir: {result}".encode())
+        else:
+            conn.send("Le système d'exploitation est incorrect.".encode())
+    elif data == "DOS:mdir toto":
+        if OS[0] == "Windows":
+            command = subprocess.run(["mdir", "toto"], capture_output=True)
+            result = command.stdout.decode('windows-1252')
+            conn.send(f"Voici le résultat de la commmande mdir toto: {result}".encode())
+        else:
+            conn.send("Le système d'exploitation est incorrect.".encode())
+    elif data == "python --version":
+        command = subprocess.run(["python", "--version"], capture_output=True)
+        result=command.stderr.decode('windows-1252')
+        conn.send(f"Voici le résultat de la commmande python --version: {result}".encode())
+    elif data == "ping 192.157.65.78":
+        if OS[0] == "Windows":
+            conn.send("ping 192.157.65.78".encode())
+        else:
+            command = subprocess.run(["ping","192.157.65.78","-c","1","-W","2"],capture_output=True)
+            result=command.stdout.decode()
+            conn.send(f"Voici le résultat de la commmande ping 192.157.65.78: {result}".encode())
+
+    else:
+        print(f"Message reçu= {data}")
+
